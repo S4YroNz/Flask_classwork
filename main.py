71931f400f3406362e255b777eaecaeabff6c627
Leamich <tennantonelove@yandex.ru>
2023-02-13 18:50:58 +0400

    Изображение Марса

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import *
 
 app = Flask(__name__)
 
@@ -21,6 +21,15 @@ def promotion():
               'И начнем с Марса!',
               'Присоединяйся!']
     return '<br>'.join(map(lambda x: '<b>' + x + '</b>', frases))
+
+
+@app.route('/image_mars')
+def image_mars():
+    return f'''<h1>Привет, Марс!</h1>
+                <img src="{url_for('static', filename='images/mars.png')}" alt="Картинка Марса">
+                <br>
+                Вот онка какая, красная планета.
+           '''
 
 
 if __name__ == '__main__':
