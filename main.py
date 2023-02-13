051a4acd73f739a70c6ab1896aafaa46fb52318f
Leamich <tennantonelove@yandex.ru>
2023-02-13 19:20:09 +0400

    Реклама с картинкой

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import *
+from random import choice
 
 app = Flask(__name__)
 
@@ -26,10 +27,44 @@ def promotion():
 @app.route('/image_mars')
 def image_mars():
     return f'''<h1>Привет, Марс!</h1>
-                <img src="{url_for('static', filename='images/mars.png')}" alt="Картинка Марса">
+                <img src="{url_for('static', filename='images/mars_2.png')}" alt="Картинка Марса">
                 <br>
                 Вот онка какая, красная планета.
            '''
+
+
+@app.route('/promotion_image')
+def promotion_image():
+    frases = ['Человечество вырастает из детства.',
+              'Человечеству мала одна планета.',
+              'Мы сделаем обитаемыми безжизненные пока планеты.',
+              'И начнем с Марса!',
+              'Присоединяйся!']
+    div_types = ['primary', 'success', 'warning', 'danger']
+    return f"""
+    <!doctype html>
+                <html lang="en">
+                  <head>
+                   <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                    <meta charset="utf-8">
+                    <title>Марс</title>
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                  </head>
+                  <body>
+                    <h1>Привет, Марс!</h1>
+                    <img src="{url_for('static', filename='images/mars_2.png')}" alt="Картинка Марса">
+                    <br>
+                    
+                    {''.join(map(lambda x: f'<div class="alert alert-{choice(div_types)}" role="alert">' + x + "</div>",
+                                 frases))}
+                    
+                  </body>
+                </html>
+            
+            """
 
 
 if __name__ == '__main__':
